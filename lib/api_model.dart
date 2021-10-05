023d47e8d4447297ef188107dac933921a4d3342
genie0806 <genie0806@gmail.com>
2021-10-05 17:35:04 +0900

    비동기를 사용해서 conference Json 받아오기

---
+++ lib/api_model.dart
@@ -0,0 +1,14 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:conference_test/model/conference_model';
+
+const conferenceUrl =
+    'https://raw.githubusercontent.com/junsuk5/mock_json/main/conferences.json';
+
+Future<List<ConferenceModel>> fetchConference() async {
+  final response = await http.get(Uri.parse(conferenceUrl));
+  Iterable jsonResponse = convert.jsonDecode(response.body);
+  List<ConferenceModel> conferences =
+      jsonResponse.map((e) => ConferenceModel.fromJson(e)).toList();
+  return conferences;
+}
