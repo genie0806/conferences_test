531eb64c5d7eb2ef5b1acdbb96eb125e9004f764
genie0806 <genie0806@gmail.com>
2021-10-05 21:39:48 +0900

    FutureBuilder list_page에 기재

--- lib/page/list_page.dart
+++ lib/page/list_page.dart
@@ -1,3 +1,7 @@
+import 'package:conference_test/api_model.dart';
+import 'package:conference_test/list_tile.dart';
+import 'package:conference_test/model/conference_model';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ConferencesList extends StatelessWidget {
@@ -8,6 +12,29 @@ class ConferencesList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Conferences'),
+      ),
+      body: FutureBuilder<List<ConferenceModel>>(
+        future: fetchConference(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text('에러가 발생했습니다.');
+          }
+          if (!snapshot.hasData) {
+            return Text('데이터가 없습니다.');
+          }
+          List<ConferenceModel> conferences = snapshot.data;
+          return ListView(children: [
+            ...conferences
+                .map((e) => ListTileForm(
+                      name: e.name,
+                      location: e.location,
+                    ))
+                .toList()
+          ]);
+        },
       ),
     );
   }
